2604ac92237360312ce5858f0193da9cd41d97bd
Mateus CohuzEr <matebatichotti@gmail.com>
2020-12-17 19:23:32 -0300

    Update ex105.py

--- Curso_em_Video_Exercicios/ex105.py
+++ Curso_em_Video_Exercicios/ex105.py
@@ -1,7 +1,27 @@
 def notas(*n):
-    lista = []
-    for i in n:
-        lista.append(n)
-    print(n)
+    lista = [n]
+    condicao = []
+    soma = 0
+    for i in lista[0]:
+        soma += i
+    quantidade = len(lista)
+    maior = max(lista)
+    menor = min(lista)
+    media = soma/quantidade
+    for i in lista[0]:
+        if i < 50:
+            condicao.append(f'Aluno {i+1}-REPROVADO')
+        elif i < 70:
+            condicao.append(f'Aluno {i+1}-RECUPERAÇÃO')
+        elif i <= 100:
+            condicao.append(f'Aluno {i+1}-APROVADO')
+        else:
+            condicao.append('<nota fora do range>')
+    respostas = [f'Foram catalogadas {quantidade} notas',
+                 f'A maior nota foi {maior}',
+                 f'A menor nota foi {menor}',
+                 f'A média foi {media}',
+                 condicao]
+    return respostas
 
-notas(23,32,13,2,23,56,67,89,57,87,87,99,100)
+print(notas(23,85,65,25,98,75,65,42,32,13,2,23,56,67,89,57,87,87,99,100))
