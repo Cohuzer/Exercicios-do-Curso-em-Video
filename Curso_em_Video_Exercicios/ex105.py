5a150cc39a6bdb49066f6455bb7176826836f9d8
Mateus CohuzEr <matebatichotti@gmail.com>
2020-12-17 19:47:42 -0300

    Update ex105.py

--- Curso_em_Video_Exercicios/ex105.py
+++ Curso_em_Video_Exercicios/ex105.py
@@ -1,4 +1,4 @@
-def notas(*n):
+def notas(*n, sit=False):
     lista = [n]
     condicao = []
     respostas = dict()
@@ -10,23 +10,26 @@ def notas(*n):
     menor = min(lista[0])
     media = soma/quantidade
 
-    c = 0
-    for i in lista[0]:
-        if i < 50:
-            condicao.append(f'Aluno {c+1}-REPROVADO')
-        elif i < 70:
-            condicao.append(f'Aluno {c+1}-RECUPERAÇÃO')
-        elif i <= 100:
-            condicao.append(f'Aluno {c+1}-APROVADO')
-        else:
-            condicao.append('<nota fora do range>')
-        c += 1
+    if sit:
+        c = 0
+        for i in lista[0]:
+            if i < 50:
+                condicao.append(f'Aluno {c+1}-REPROVADO')
+            elif i < 70:
+                condicao.append(f'Aluno {c+1}-RECUPERAÇÃO')
+            elif i <= 100:
+                condicao.append(f'Aluno {c+1}-APROVADO')
+            else:
+                condicao.append('<nota fora do range>')
+            c += 1
 
     respostas['Quantidade'] = quantidade
     respostas['Maior Nota'] = maior
     respostas['Menor Nota'] = menor
     respostas['Media das Notas'] = media
-    respostas['Condição'] = condicao
+    if sit:
+        respostas['Condição'] = condicao
+
     return respostas
 
-print(notas(23,85,65,25,98,75,65,42,32,13,2,23,56,67,89,57,87,87,99,100))
+print(notas(23,85,65,25,98,75,65,42,32,13,2,23,56,67,89,57,87,87,99,100, sit=True))
