f9f8942f7067c2fe7e6101f56deab6d02d10c639
Mateus CohuzEr <matebatichotti@gmail.com>
2020-12-18 12:09:46 -0300

    Update ex106.py

--- Curso_em_Video_Exercicios/ex106.py
+++ Curso_em_Video_Exercicios/ex106.py
@@ -1,3 +1,5 @@
-print('=-'*30)
-print(f'{"A.H.P.- Assistente HELP() em Python": ^60}')
-print('=-'*30)
+while True:
+    print('\033[01;47m=-'*30)
+    print(f'{"A.H.P.- Assistente HELP() em Python": ^60}')
+    print('=-'*30)
+    break
