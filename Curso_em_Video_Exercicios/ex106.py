b7dcba7cc02db386f6bc9cd2b30dfae02d3cc9fc
Mateus CohuzEr <matebatichotti@gmail.com>
2020-12-18 12:37:33 -0300

    Update ex106.py

--- Curso_em_Video_Exercicios/ex106.py
+++ Curso_em_Video_Exercicios/ex106.py
@@ -1,5 +1,9 @@
+from random import randint
 while True:
-    print('\033[01;47m=-'*30)
+    print('\033[01;43m=-'*30)
     print(f'{"A.H.P.- Assistente HELP() em Python": ^60}')
     print('=-'*30)
-    break
+    funcao = str(input(f'\033[m -> Insira o nome da função/biblioteca: \033[01;{randint(41,47)}m'))
+    if funcao.lower() == 'fim':
+        break
+    print(f'{help(funcao)}')
