d37d9052ed7d0d2e39be0c872dac78f25acaee32
Mateus CohuzEr <matebatichotti@gmail.com>
2020-12-18 13:29:09 -0300

    Update ex103.py

--- Curso_em_Video_Exercicios/ex103.py
+++ Curso_em_Video_Exercicios/ex103.py
@@ -1,16 +1,16 @@
 def ficha(nome=None, gols=None):
     dicionario = dict()
-    if nome != None and gols != None:
-        dicionario[f'{nome}'] = gols
-    elif nome == None and gols != None:
-        dicionario['<Não catalogado>'] = gols
-    elif nome != None and gols == None:
-        dicionario[f'{nome}'] = '<Não catalogado>'
+    if nome == None:
+        nome = '<Não catalogado>'
+    if gols == None:
+        gols = '<Não catalogado>'
+    dicionario[nome] = gols
     return dicionario
 
 nome = str(input('Nome do jogador: '))
 gols = int(input('Gols do jogador: '))
 dicionario = ficha(nome, gols)
+
 for i in dicionario.keys():
     for j in dicionario.values():
         print(f'O jogador {i} fez {j} gols')
