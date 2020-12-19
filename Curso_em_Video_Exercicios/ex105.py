cb9977797006f21f8f42af5be7b3cb4d438359e6
Mateus CohuzEr <matebatichotti@gmail.com>
2020-12-19 19:47:06 -0300

    Update ex105.py

--- Curso_em_Video_Exercicios/ex105.py
+++ Curso_em_Video_Exercicios/ex105.py
@@ -3,7 +3,7 @@ def notas(*n, sit=False):
     => A função analisa as notas de um número não fixo de notas
     :param n: notas dos alunos (Tupla);
     :param sit: Booleano para definir aparição ou não da situação de cada aluno;
-    :return: retorna a quantidade de notas cadastradas, maior e menor nota, média e situação.
+    :return: retorna a quantidade de notas cadastradas, maior e menor nota, média, situação e a situação geral.
     """
     lista = [n]
     condicao = []
@@ -14,27 +14,39 @@ def notas(*n, sit=False):
     quantidade = len(lista[0])
     maior = max(lista[0])
     menor = min(lista[0])
-    media = soma/quantidade
+    media = soma / quantidade
+    condicaogeral = 'ERRO'
 
     if sit:
         c = 0
         for i in lista[0]:
             if i < 50:
-                condicao.append(f'Aluno {c+1}-REPROVADO')
+                condicao.append(f'Aluno {c + 1}-REPROVADO')
             elif i < 70:
-                condicao.append(f'Aluno {c+1}-RECUPERAÇÃO')
+                condicao.append(f'Aluno {c + 1}-RECUPERAÇÃO')
             elif i <= 100:
-                condicao.append(f'Aluno {c+1}-APROVADO')
+                condicao.append(f'Aluno {c + 1}-APROVADO')
             else:
                 condicao.append('<nota fora do range>')
             c += 1
+        if media < 50:
+            condicaogeral = f'PÉSSIMA - {media:.2f}'
+        elif media < 70:
+            condicaogeral = f'RUIM - {media:.2f}'
+        elif media < 100:
+            condicaogeral = f'ÓTIMA - {media:.2f}'
+        else:
+            condicaogeral = f'ERRO - {media:.2f}'
 
     respostas['Quantidade'] = quantidade
     respostas['Maior Nota'] = maior
     respostas['Menor Nota'] = menor
     respostas['Media das Notas'] = media
-    if sit: respostas['Condição'] = condicao
+    if sit:
+        respostas['Condição'] = condicao
+        respostas['Condição Geral'] = condicaogeral
 
     return respostas
 
-print(notas(23,85,65,25,98,75,65,42,32,13,2,23,56,67,89,57,87,87,99,100, sit=True))
+
+print(notas(23, 85, 65, 25, 98, 2.1, 32.1, 90.4, 75, 65, 42, 32, 13, 2, 23, 56, 67, 89, 57, 87, 87, 99, 100, sit=True))
