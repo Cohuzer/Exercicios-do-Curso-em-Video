1ad194eeb6300e749fb2465ed7da787337c72e93
Mateus CohuzEr <matebatichotti@gmail.com>
2020-12-19 19:30:21 -0300

    Update ex104.py

--- Curso_em_Video_Exercicios/ex104.py
+++ Curso_em_Video_Exercicios/ex104.py
@@ -1,6 +1,12 @@
-def leiaInt(n):
-    n = input('Digitealgo: ')
-    if n in '0123456789':
-        return True
-    else:
-        return False
+def leiaInt(entrada):
+    n = str(input(entrada)).strip()
+
+    while not n.isnumeric():
+        print('\033[1;31mERRO! Digite um número inteiro valido!\033[m')
+        n = str(input(entrada)).strip()
+
+    n = int(n)
+    return n
+
+n = leiaInt('Digite um número: ')
+print(f'Você digitou o número {n}')
