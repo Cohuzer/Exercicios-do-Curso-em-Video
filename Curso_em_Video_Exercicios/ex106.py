3f2033574b2e4bd3dfd66c8d01cf4d4a58305857
Mateus CohuzEr <matebatichotti@gmail.com>
2020-12-19 19:54:29 -0300

    Update ex106.py

--- Curso_em_Video_Exercicios/ex106.py
+++ Curso_em_Video_Exercicios/ex106.py
@@ -9,7 +9,7 @@ def AHP():
     from time import sleep
     while True:
         formatar('A.H.P. <-> Assistente HELPY-ME Please')
-        funcao = str(input(f'\033[m -> Insira o nome da função/biblioteca: '))
+        funcao = str(input(f'\033[m -> Insira o nome da função/biblioteca >>> '))
         if funcao.lower() == 'fim':
             formatar('Volte sempre!')
             break
