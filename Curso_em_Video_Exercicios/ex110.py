ee125763593c7a67406ca1fc6cfc002de3b07852
Mateus CohuzEr <matebatichotti@gmail.com>
2020-12-30 15:01:08 -0300

    ex110

---
+++ Curso_em_Video_Exercicios/ex110.py
@@ -0,0 +1,4 @@
+import moeda
+
+num = float(input('Digite um número: '))
+moeda.resumo(num, 80, 40, True)
--- Curso_em_Video_Exercicios/moeda.py
+++ Curso_em_Video_Exercicios/moeda.py
@@ -33,6 +33,18 @@ def metade(numero, sit=False):
     else:
         return resultado
 
+
 def moeda(numero):
     textoFormatado = f'R${numero:.2f}'.replace('.', ',')
     return textoFormatado
+
+
+def resumo(numero, porcentagemAumento, porcentagemDiminuir, sit=False):
+    print(f'-=' * 15)
+    print(f"{'RESUMO': ^30}")
+    print(f'-=' * 15)
+    print(f'  >>> Aumentar = {aumentar(numero, porcentagemAumento, sit)};\n'
+          f'  >>> Diminuir = {diminuir(numero, porcentagemDiminuir, sit)};\n'
+          f'  >>> Dobro = {dobro(numero, sit)};\n'
+          f'  >>> Metade = {metade(numero, sit)}')
+
