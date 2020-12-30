700a8946419a7d1824e6975eb3b27e8fbbd2fcd5
Mateus CohuzEr <matebatichotti@gmail.com>
2020-12-30 14:29:19 -0300

    Update ex107.py

--- Curso_em_Video_Exercicios/ex107.py
+++ Curso_em_Video_Exercicios/ex107.py
@@ -6,4 +6,4 @@ por = 40
 print(f'Aumentar = {moeda.aumentar(num, por)};\n'
       f'Diminuir = {moeda.diminuir(num, por)};\n'
       f'Dobro = {moeda.dobro(num)};\n'
-      f'Metade = {moeda.metade()}.')
+      f'Metade = {moeda.metade(num)}.')
