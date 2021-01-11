ea12da2b2652539a43cbd85abded807d2cc68b2a
Mateus CohuzEr <matebatichotti@gmail.com>
2021-01-11 14:35:11 -0300

    Update ex113.py

--- Curso_em_Video_Exercicios/ex113.py
+++ Curso_em_Video_Exercicios/ex113.py
@@ -1,9 +1,17 @@
+from typing import Type
+
+
 def leiaInt(entrada):
     while True:
         try:
             n = int(input(entrada))
-        except (ValueError, TypeError, KeyboardInterrupt):
-            print('\033[31mERRO! Digite um valor valido\033[m')
+        except (ValueError):
+            print('\033[31mVALOR INVALIDO! Digite um valor valido\033[m')
+        except(TypeError):
+            print(f'\033[31mVALOR INVALIDO! Informe um número valido!\033[m')
+        except(KeyboardInterrupt):
+            print(f'O usuario preferiu não informar esse número!')
+            return 0
         else:
             break
 
@@ -13,8 +21,13 @@ def leiaFloat(entrada):
     while True:
         try:
             n2 = float(input(entrada))
-        except (ValueError, TypeError, KeyboardInterrupt):
-            print('\033[31mERRO! Digite um valor valido\033[m')
+        except (ValueError):
+            print('\033[31mVALOR INVALIDO! Digite um valor valido\033[m')
+        except(TypeError):
+            print(f'\033[31mVALOR INVALIDO! Informe um número valido!\033[m')
+        except(KeyboardInterrupt):
+            print(f'O usuario preferiu não informar esse número!')
+            return 0
         else:
             break
     
